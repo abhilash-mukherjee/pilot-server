26a7568a58568ed3dcae9a80495ee9ca30194e3d
Abhilash <abhilash301267@gmail.com>
2024-03-23 23:02:50 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from 'express';
+const app = express();
+app.get('/hello-world', (req: Request, res: Response) => {
+  res.send('Hello, World!');
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
